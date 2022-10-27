6484094ade0e93a2b0914c808e8befcb659892f5
Shivank19 <shivank.sapra@gmail.com>
2022-10-28 00:44:55 +0530

    Project Upload

---
+++ Index.js
@@ -0,0 +1,123 @@
+// SHOW MENU
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu');
+    });
+  }
+};
+showMenu('nav-toggle', 'nav-menu');
+
+//REMOVE MENU MOBILE
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+//SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+// SHOW SCROLL TO TOP
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll__top');
+  if (this.scrollY >= 200) scrollTop.classList.add('show-scroll');
+  else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
+
+//TOGGLE THEME
+const themeButton = document.getElementById('theme-btn');
+const darkTheme = 'light-theme';
+const iconTheme = 'bx-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+//SCALE DOWN TO PRINT
+function scaleDown() {
+  document.body.classList.add('scale-down');
+}
+
+//RESET AFTER DOWNLOADING
+function resetScale() {
+  document.body.classList.remove('scale-down');
+}
+
+// GENERATE PDF
+let areaCV = document.getElementById('area-cv');
+let resumeButton = document.getElementById('resume-btn');
+
+let opt = {
+  margin: 0,
+  filename: 'ShivankSapra_Resume.pdf',
+  image: { type: 'jpeg', quality: 0.9 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: 'a4', orientation: 'portrait' },
+};
+
+function generateCV() {
+  html2pdf(areaCV, opt);
+}
+
+resumeButton.addEventListener('click', () => {
+  scaleDown();
+  generateCV();
+  setTimeout(resetScale, 5000);
+});
